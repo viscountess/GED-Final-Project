2b8252b81e93a32ddb72b8493f902bd5ed3af93a
viscountess <venerable_viscountess@hotmail.com>
2015-01-18 00:55:08 +0000

    Create StateCharacterScreen.h
    header file for character selection screen

---
+++ StateCharacterScreen.h
@@ -0,0 +1,31 @@
+#ifndef statecharacterscreen_INCLUDE
+#define statecharacterscreen_INCLUDE
+
+#include "Game.h"
+
+//StateMainMenu inherits GameState class
+class StateCharacterScreen :public GameState{
+public:
+	StateCharacterScreen(); //constructor
+	virtual ~StateCharacterScreen(); //deconstructor
+
+	virtual void init();
+	virtual void draw(SDL_Window * window); //draw the game state 
+	virtual void handleEvent(SDL_Event const &sdlEvent, Game &context); //method to handle what state the game is at
+	virtual void update(Game &context); //update the gamestate
+	virtual void Cleanup(); //delete everything that is unused so that memory can be freed
+	void displayMenuText(); //method to draw the menu text on the menu state
+	virtual void enter();
+
+private:
+	//pointers for the text displayed on the main menu
+	Label* maleLabel;
+	Label* femaleLabel;
+
+	TTF_Font* textFont;	// SDL type for True-Type font rendering
+
+	int timeOfEnteredState;
+
+};
+
+#endif
