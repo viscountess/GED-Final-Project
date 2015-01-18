2945cc3b30fac4c4e3d933b98db8470ce093b1f1
viscountess <venerable_viscountess@hotmail.com>
2015-01-18 01:26:00 +0000

    Update AbstractNPC.h
    Added in AbstractNPC base class

--- AbstractNPC.h
+++ AbstractNPC.h
@@ -2,3 +2,18 @@
 #define abstractnpc_INCLUDE
 
 #endif //abstractnpc_INCLUDE
+
+
+//Abstract base class: no implementation
+//Base class for Monster Design Pattern
+class AbstractNPC{
+public:
+	virtual void render() = 0;
+	virtual int getMana() = 0; 
+	virtual int getHealth() = 0;
+	virtual int getStrength() = 0;
+	virtual int getRefreshRate() = 0;
+	virtual int getDropRate() = 0;
+	virtual ~AbstractNPC(){}
+
+};
