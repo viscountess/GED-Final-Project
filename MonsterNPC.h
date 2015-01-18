02f3918771057392a7c016ee795fe05b43caa76e
viscountess <venerable_viscountess@hotmail.com>
2015-01-18 01:53:53 +0000

    Updating the MonsterNPC to inherit the AbstractNPC class

--- MonsterNPC.h
+++ MonsterNPC.h
@@ -1,4 +1,17 @@
 #ifndef monsternpc_INCLUDE
 #define monsternpc_INCLUDE
 
+#include "AbstractNPC.h"
+
+//MonsterNPC implements AbstractNPC - all methods defined inline
+class MonsterNPC : public AbstractNPC{
+public:
+	virtual void render();
+	virtual int getMana();
+	virtual int getHealth();
+	virtual int getStrength();
+	virtual int getRefreshRate();
+	virtual int getDropRate();
+};
+
 #endif //monsternpc_INCLUDE
